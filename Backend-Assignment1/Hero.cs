5057a4a4ecf5923e5796b786b2e2a3dcc5294462
Vanessa Pastén-Millán <vane.tama@hotmail.com>
2023-02-09 13:50:04 +0100

    Add project files.

---
+++ Backend-Assignment1/Hero.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend_Assignment1
+{
+    internal abstract class Hero
+    {
+        protected string name;
+        protected int level = 1;
+        protected levelAttributes = 
+        protected Hero(string name)
+        {
+            this.name = name;   
+        }
+    }
+}
