798b92853b4e3dab22f75e5a6c9b4662128dd4ac
Vanessa Pastén-Millán <vane.tama@hotmail.com>
2023-02-18 02:27:33 +0100

    feat: remove code inside Main method

--- Backend-Assignment1/Program.cs
+++ Backend-Assignment1/Program.cs
@@ -6,14 +6,6 @@ namespace Backend_Assignment1
 {
     internal class Program
     {
-        static void Main(string[] args) {
-            Mage mage = new("");
-            Weapon weapon = new("", 0, WeaponType.Staffs, 1);
-            Armor armor = new("",0, BackendAssignment1.Enums.Slot.Legs, ArmorType.Cloth, new(2, 2, 2));
-            mage.Equip(weapon);
-            mage.Equip(armor);
-            Console.WriteLine(mage.Damage(mage.TotalAttributes()));
-
-        }
+        static void Main(string[] args) {}
     }
 }
